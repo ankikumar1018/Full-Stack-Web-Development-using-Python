ae57c2f7ae0a6323313cd47aa804e7b3fa69e2c4
Ankit Kumar <aktetri@gmail.com>
2022-10-26 09:13:37 +0530

    Add files via upload

---
+++ Assignment -2  Python Basics/2.py
@@ -0,0 +1,10 @@
+''' Write a python script to add multi line comments and print values of four variables, 
+each in a new line. Variable contains any values. '''
+a = 'Ankit'
+b = 25
+c = 25.5
+d = 'End'
+print(a)
+print(b)
+print(c)
+print(d)
