cca43740ddca3fce1fc3c72eeca22844861dcced
Ankit Kumar <aktetri@gmail.com>
2022-11-01 10:05:19 +0530

    Add files via upload

---
+++ Assignment-11 loops/3.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a Number: "))
+sum_num = 0
+for i in range(1, n + 1):
+    sum_num += (i ** 3)
+
+print(sum_num)
---
+++ Assignment-11 loops/6.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a Number: "))
+fact_num = 1
+for i in range(1, n + 1):
+    fact_num *= i
+
+print(fact_num)
